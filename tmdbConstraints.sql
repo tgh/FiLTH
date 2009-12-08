d7b25859cd41b2f7a1c069bd6cd3d0acaa981cdf
Tyler Hayes <tgh@pdx.edu>
2009-12-08 10:14:09 -0800

    refining the schema

--- tmdbConstraints.sql
+++ tmdbConstraints.sql
@@ -2,36 +2,82 @@
 -- Primary Key constraints --
 -----------------------------
 
-ALTER TABLE movie ADD CONSTRAINT movie_pkey PRIMARY KEY(title, year);
---ALTER TABLE genre ADD CONSTRAINT genre_pkey PRIMARY KEY(genname);
+ALTER TABLE movie ADD CONSTRAINT movie_pkey PRIMARY KEY(movieid);
+ALTER TABLE crewperson ADD CONSTRAINT crew_pkey PRIMARY KEY(crewid);
+ALTER TABLE workedon ADD CONSTRAINT worked_pkey PRIMARY KEY(movieid, crewid, position);
+ALTER TABLE genre ADD CONSTRAINT genre_pkey PRIMARY KEY(genname);
+ALTER TABLE genrecontains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(movieid, genname);
+ALTER TABLE oscar ADD CONSTRAINT oscar_pkey PRIMARY KEY (category);
+ALTER TABLE oscargivento ADD CONSTRAINT oscargiven_pkey PRIMARY KEY(movieid, category);
+ALTER TABLE list ADD CONSTRAINT list_pkey PRIMARY KEY(listtitle);
+ALTER TABLE listcontains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(movieid, listtitle);
+ALTER TABLE country ADD CONSTRAINT country_pkey PRIMARY KEY(countryname);
+/*
 ALTER TABLE director ADD CONSTRAINT director_pkey PRIMARY KEY(dirname);
 ALTER TABLE actor ADD CONSTRAINT actor_pkey PRIMARY KEY(actname);
 ALTER TABLE screenwriter ADD CONSTRAINT screenwriter_pkey PRIMARY KEY(scrname);
 ALTER TABLE cinematographer ADD CONSTRAINT cinematographer_pkey PRIMARY KEY(cinname);
-ALTER TABLE oscar ADD CONSTRAINT oscar_pkey PRIMARY KEY(title, year, category);
-ALTER TABLE list ADD CONSTRAINT list_pkey PRIMARY KEY(listtitle);
 ALTER TABLE directed ADD CONSTRAINT directed_pkey PRIMARY KEY(title, year, dirname);
 ALTER TABLE actedin ADD CONSTRAINT actedin_pkey PRIMARY KEY(title, year, actname);
 ALTER TABLE wrote ADD CONSTRAINT wrote_pkey PRIMARY KEY(title, year, scrname);
 ALTER TABLE shot ADD CONSTRAINT shot_pkey PRIMARY KEY(title, year, cinname);
-ALTER TABLE listcontains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(title, year, listtitle);
---ALTER TABLE genrecontains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(title, year, genname);
---ALTER TABLE country ADD CONSTRAINT country_pkey PRIMARY KEY(countryname);
+*/
 
 -----------------------------
 -- Foreign Key constraints --
 -----------------------------
 
--- movie table
+-- movie table country FK
 ALTER TABLE movie ADD CONSTRAINT movie_country_fkey
 FOREIGN KEY (country) REFERENCES country(countryname)
+ON UPDATE CASCADE ON DELETE SET NULL;
+
+-- workedon table movie FK
+ALTER TABLE workedon ADD CONSTRAINT worked_mov_fkey
+FOREIGN KEY (movieid) REFERENCES movie(movieid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
--- oscar table
+-- worked on table crew FK
+ALTER TABLE workedon ADD CONSTRAINT worked_crew_fkey
+FOREIGN KEY (crewid) REFERENCES crewperson(crewid)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- genrecontains table movie FK
+ALTER TABLE genrecontains ADD CONSTRAINT genre_movie_fkey
+FOREIGN KEY (movieid) REFERENCES movie(movieid)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- genrecontains table genre FK
+ALTER TABLE genrecontains ADD CONSTRAINT genre_genname_fkey
+FOREIGN KEY (genname) REFERENCES genre(genname)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- oscargivento table movie FK
 ALTER TABLE oscar ADD CONSTRAINT oscar_movie_fkey
-FOREIGN KEY (title, year) REFERENCES movie(title, year)
+FOREIGN KEY (movieid) REFERENCES movie(movieid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
+-- oscargivento table recipient FK
+ALTER TABLE oscar ADD CONSTRAINT oscar_recip_fkey
+FOREIGN KEY (recipient) REFERENCES crewperson(crewid)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- oscargivento table category FK
+ALTER TABLE oscar ADD CONSTRAINT oscar_cat_fkey
+FOREIGN KEY (category) REFERENCES oscar(category)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- listcontains table movie FK
+ALTER TABLE listcontains ADD CONSTRAINT list_movie_fkey
+FOREIGN KEY (movieid) REFERENCES movie(movieid)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- listcontains table list FK
+ALTER TABLE listcontains ADD CONSTRAINT list_title_fkey
+FOREIGN KEY (listtitle) REFERENCES list(listtitle)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+/*
 -- directed table
 ALTER TABLE directed ADD CONSTRAINT dir_movie_fkey
 FOREIGN KEY (title, year) REFERENCES movie(title, year)
@@ -67,21 +113,8 @@ ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE shot ADD CONSTRAINT shot_act_fkey
 FOREIGN KEY (cinname) REFERENCES cinematographer(cinname)
 ON UPDATE CASCADE ON DELETE CASCADE;
-
--- listcontains table
-ALTER TABLE listcontains ADD CONSTRAINT list_movie_fkey
-FOREIGN KEY (title, year) REFERENCES movie(title, year)
-ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE listcontains ADD CONSTRAINT list_act_fkey
-FOREIGN KEY (listtitle) REFERENCES list(listtitle)
-ON UPDATE CASCADE ON DELETE CASCADE;
-/*
--- genrecontains table
-ALTER TABLE genrecontains ADD CONSTRAINT genre_movie_fkey
-FOREIGN KEY (title, year) REFERENCES movie(title, year)
-ON UPDATE CASCADE ON DELETE CASCADE;
 */
+
 ---------------------------
 -- Integrity constraints --
 ---------------------------
@@ -102,34 +135,23 @@ CHECK (mystarrating = 'NO STARS' OR
        mystarrating = '***&frac12' OR
        mystarrating = '****' OR
        mystarrating = 'N/A' OR
-       mystarrating = 'Haven''t seen');
+       mystarrating = '[not seen]');
+
+-- movie mpaa rating
+ALTER TABLE movie ADD CONSTRAINT mpaa_constraint
+CHECK (mpaa = 'NR' OR
+       mpaa = 'G' OR
+       mpaa = 'PG' OR
+       mpaa = 'PG-13' OR
+       mpaa = 'R' OR
+       mpaa = 'X' OR
+       mpaa = 'NC-17');
 
 -- listcontains rank
 ALTER TABLE listcontains ADD CONSTRAINT rank_constraint
 CHECK (rankinlist > 0);
 
--- oscar category
-ALTER TABLE oscar ADD CONSTRAINT category_constraint
-CHECK (category = 'Best Picture' OR
-       category = 'Best Actor' OR
-       category = 'Best Actress' OR
-       category = 'Best Supporting Actor' OR
-       category = 'Best Supporting Actress' OR
-       category = 'Best Director' OR
-       category = 'Best Cinematography' OR
-       category = 'Best Adapted Screenplay' OR
-       category = 'Best Original Screenplay' OR
-       category = 'Best Foreign Language Film');
-
--- oscar status
-ALTER TABLE oscar ADD CONSTRAINT status_constraint
+-- oscargivento status
+ALTER TABLE oscargivento ADD CONSTRAINT osc_stat_constraint
 CHECK (status = 'won' OR
        status = 'nominated');
-
--- directed director exists
---ALTER TABLE directed ADD CONSTRAINT director_constraint
---CHECK (EXISTS (SELECT *
---               FROM crewperson, directed
---               WHERE fname = dirfname AND
---                     lname = dirlname AND
---                     position = 'Director'));
--- tmdbTables.sql
+++ tmdbTables.sql
@@ -3,20 +3,59 @@
 -----------------------
 
 CREATE TABLE movie (
+movieid serial NOT NULL,
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 mystarrating varchar(11) DEFAULT NULL,
 country varchar(30) DEFAULT NULL,
 mpaa varchar(7) DEFAULT NULL);
-/*
+
 CREATE TABLE crewperson (
+crewid serial NOT NULL,
 fname varchar(15) NOT NULL,
-lname varchar(20) NOT NULL,
-position varchar(20) NOT NULL);
+lname varchar(20) NOT NULL);
+
+CREATE TABLE workedon (
+movieid integer NOT NULL,
+crewid integer NOT NULL,
+position varhcar(20) NOT NULL);
 
 CREATE TABLE genre (
 genname varchar(25) NOT NULL);
-*/
+
+CREATE TABLE genrecontains (
+movieid integer NOT NULL,
+genname varchar(25) NOT NULL);
+
+CREATE TABLE oscar (
+category varchar(25) NOT NULL);
+
+CREATE TABLE oscargivento (
+movieid integer NOT NULL,
+category varchar(25) NOT NULL,
+recipient integer NOT NULL,
+status varchar(10) DEFAULT NULL);
+
+CREATE TABLE list (
+listtitle varchar(50) NOT NULL,
+listauthor varchar(25) DEFAULT NULL);
+
+CREATE TABLE listcontains (
+movieid integer NOT NULL,
+listtitle varchar(50) NOT NULL,
+rankinlist integer DEFAULT NULL);
+
+CREATE TABLE country (
+countryname varchar(30) NOT NULL);
+
+/*
+CREATE TABLE movie (
+title varchar(100) NOT NULL,
+year integer NOT NULL,
+mystarrating varchar(11) DEFAULT NULL,
+country varchar(30) DEFAULT NULL,
+mpaa varchar(7) DEFAULT NULL);
+
 CREATE TABLE director (
 dirname varchar(25) NOT NULL);
 
@@ -35,74 +74,47 @@ year integer NOT NULL,
 category varchar(25) NOT NULL,
 recipientName varchar(25) DEFAULT NULL,
 status varchar(10) DEFAULT NULL);
-/*
-CREATE TABLE oscar (
-title varchar(100) NOT NULL,
-year integer NOT NULL,
-category varchar(25) NOT NULL,
-recipientfname varchar(15) NOT NULL,
-recipientlname varchar(20) NOT NULL,
-status varchar(10) DEFAULT NULL);
-*/
-CREATE TABLE list (
-listtitle varchar(50) NOT NULL,
-listauthor varchar(25) DEFAULT NULL);
 
 CREATE TABLE directed (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 dirname varchar(25) NOT NULL);
-/*
+
 CREATE TABLE directed (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 dirfname varchar(15) NOT NULL,
 dirlname varchar(20) NOT NULL);
-*/
+
 CREATE TABLE actedin (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
-actname varchar(25) NOT NULL);
-/*
+
 CREATE TABLE actedin (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 actfname varchar(15) NOT NULL,
 actlname varchar(20) NOT NULL);
-*/
+
 CREATE TABLE wrote (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 scrname varchar(25) NOT NULL);
-/*
+
 CREATE TABLE wrote (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 scrfname varchar(15) NOT NULL,
 scrlname varchar(20) NOT NULL);
-*/
+
 CREATE TABLE shot (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 cinname varchar(25) NOT NULL);
-/*
+
 CREATE TABLE shot (
 title varchar(100) NOT NULL,
 year integer NOT NULL,
 cinfname varchar(15) NOT NULL,
 cinlname varchar(20) NOT NULL);
-*/
-CREATE TABLE listcontains (
-title varchar(100) NOT NULL,
-year integer NOT NULL,
-listtitle varchar(50) NOT NULL,
-rankinlist integer DEFAULT NULL);
-/*
-CREATE TABLE genrecontains (
-title varchar(100) NOT NULL,
-year integer NOT NULL,
-genname varchar(25) NOT NULL);
-
-CREATE TABLE country (
-countryname varchar(30) NOT NULL);
 */
