d6f139929615f112c68134ab66d92076e9dfee36
Tyler Hayes <tgh@pdx.edu>
2010-01-22 09:10:37 -0800

    ENTIRE DIRECTORY RESTRUCTURING

--- tmdbOscar.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO oscar VALUES ('Best Actor');
-INSERT INTO oscar VALUES ('Best Actress');
-INSERT INTO oscar VALUES ('Best Supporting Actor');
-INSERT INTO oscar VALUES ('Best Supporting Actress');
-INSERT INTO oscar VALUES ('Best Director');
-INSERT INTO oscar VALUES ('Best Cinematography');
-INSERT INTO oscar VALUES ('Best Adapted Screenplay');
-INSERT INTO oscar VALUES ('Best Original Screenplay');
-INSERT INTO oscar VALUES ('Best Foreign Language Film');
