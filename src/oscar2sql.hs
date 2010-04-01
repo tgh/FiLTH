b5d84ce91c527a42dacd8c79f768737811627d6f
Tyler Hayes <tgh@pdx.edu>
2010-04-01 09:53:01 -0700

    added oscar2sql and resulting oscar.sql

---
+++ src/oscar2sql.hs
@@ -0,0 +1,20 @@
+module Main where
+import System.Environment (getArgs)
+import System.IO
+
+
+main = do
+  (file:args) <- getArgs
+  contents <- readFile file
+  let countries = lines contents
+      -- e.g. ["Best Picture","Best Actor",...]
+      sqlStatements = toSql countries
+  mapM putStr sqlStatements
+
+
+
+toSql :: [String] -> [String]
+toSql categories = map sqlConversion categories
+
+sqlConversion :: String -> String
+sqlConversion category = "INSERT INTO oscar VALUES ('" ++ category ++ "');\n"
