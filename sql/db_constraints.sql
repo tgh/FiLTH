ac9e7e244a905bd50a8152fb2823dc751bebd46e
Tyler Hayes <tgh@pdx.edu>
2010-11-04 20:08:50 -0700

    Change naming convention od db
    to words separated by underscores.  Completes tasks #23.

--- sql/db_constraints.sql
+++ sql/db_constraints.sql
@@ -2,15 +2,15 @@
 -- Primary Key constraints --
 -----------------------------
 
-ALTER TABLE movie ADD CONSTRAINT movie_pkey PRIMARY KEY(movieid);
-ALTER TABLE crewperson ADD CONSTRAINT crew_pkey PRIMARY KEY(crewid);
-ALTER TABLE workedon ADD CONSTRAINT worked_pkey PRIMARY KEY(movieid, crewid, position);
+ALTER TABLE movie ADD CONSTRAINT movie_pkey PRIMARY KEY(mid);
+ALTER TABLE crew_person ADD CONSTRAINT crew_pkey PRIMARY KEY(cid);
+ALTER TABLE worked_on ADD CONSTRAINT worked_pkey PRIMARY KEY(mid, cid, position);
 ALTER TABLE genre ADD CONSTRAINT genre_pkey PRIMARY KEY(genname);
-ALTER TABLE genrecontains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(movieid, genname);
+ALTER TABLE genre_contains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(mid, genname);
 ALTER TABLE oscar ADD CONSTRAINT oscar_pkey PRIMARY KEY (category);
-ALTER TABLE oscargivento ADD CONSTRAINT oscargiven_pkey PRIMARY KEY(movieid, category);
+ALTER TABLE oscar_given_to ADD CONSTRAINT oscargiven_pkey PRIMARY KEY(mid, category);
 ALTER TABLE list ADD CONSTRAINT list_pkey PRIMARY KEY(listtitle);
-ALTER TABLE listcontains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(movieid, listtitle);
+ALTER TABLE list_contains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(mid, listtitle);
 ALTER TABLE country ADD CONSTRAINT country_pkey PRIMARY KEY(countryname);
 
 -----------------------------
@@ -19,37 +19,37 @@ ALTER TABLE country ADD CONSTRAINT country_pkey PRIMARY KEY(countryname);
 
 -- movie table country FK
 ALTER TABLE movie ADD CONSTRAINT movie_country_fkey
-FOREIGN KEY (country) REFERENCES country(countryname)
+FOREIGN KEY (country) REFERENCES country(country_name)
 ON UPDATE CASCADE ON DELETE SET NULL;
 
 -- workedon table movie FK
-ALTER TABLE workedon ADD CONSTRAINT worked_mov_fkey
-FOREIGN KEY (movieid) REFERENCES movie(movieid)
+ALTER TABLE worked_on ADD CONSTRAINT worked_mov_fkey
+FOREIGN KEY (mid) REFERENCES movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- worked on table crew FK
-ALTER TABLE workedon ADD CONSTRAINT worked_crew_fkey
-FOREIGN KEY (crewid) REFERENCES crewperson(crewid)
+ALTER TABLE worked_on ADD CONSTRAINT worked_crew_fkey
+FOREIGN KEY (cid) REFERENCES crew_person(cid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- genrecontains table movie FK
-ALTER TABLE genrecontains ADD CONSTRAINT genre_movie_fkey
-FOREIGN KEY (movieid) REFERENCES movie(movieid)
+ALTER TABLE genre_contains ADD CONSTRAINT genre_movie_fkey
+FOREIGN KEY (mid) REFERENCES movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- genrecontains table genre FK
-ALTER TABLE genrecontains ADD CONSTRAINT genre_genname_fkey
-FOREIGN KEY (genname) REFERENCES genre(genname)
+ALTER TABLE genre_contains ADD CONSTRAINT genre_genname_fkey
+FOREIGN KEY (gen_name) REFERENCES genre(gen_name)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscargivento table movie FK
 ALTER TABLE oscar ADD CONSTRAINT oscar_movie_fkey
-FOREIGN KEY (movieid) REFERENCES movie(movieid)
+FOREIGN KEY (mid) REFERENCES movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscargivento table recipient FK
 ALTER TABLE oscar ADD CONSTRAINT oscar_recip_fkey
-FOREIGN KEY (recipient) REFERENCES crewperson(crewid)
+FOREIGN KEY (recipient) REFERENCES crew_person(cid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscargivento table category FK
@@ -58,13 +58,13 @@ FOREIGN KEY (category) REFERENCES oscar(category)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- listcontains table movie FK
-ALTER TABLE listcontains ADD CONSTRAINT list_movie_fkey
-FOREIGN KEY (movieid) REFERENCES movie(movieid)
+ALTER TABLE list_contains ADD CONSTRAINT list_movie_fkey
+FOREIGN KEY (mid) REFERENCES movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- listcontains table list FK
-ALTER TABLE listcontains ADD CONSTRAINT list_title_fkey
-FOREIGN KEY (listtitle) REFERENCES list(listtitle)
+ALTER TABLE list_contains ADD CONSTRAINT list_title_fkey
+FOREIGN KEY (list_title) REFERENCES list(list_title)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 ---------------------------
@@ -77,17 +77,17 @@ CHECK (year >= 1900 AND year <= 2012);
 
 -- movie star rating
 ALTER TABLE movie ADD CONSTRAINT star_constraint
-CHECK (mystarrating = 'NO STARS' OR
-       mystarrating = '&frac12*' OR
-       mystarrating = '*' OR
-       mystarrating = '*&frac12' OR
-       mystarrating = '**' OR
-       mystarrating = '**&frac12' OR
-       mystarrating = '***' OR
-       mystarrating = '***&frac12' OR
-       mystarrating = '****' OR
-       mystarrating = 'N/A' OR
-       mystarrating = '[not seen]');
+CHECK (my_star_rating = 'NO STARS' OR
+       my_star_rating = '&frac12*' OR
+       my_star_rating = '*' OR
+       my_star_rating = '*&frac12' OR
+       my_star_rating = '**' OR
+       my_star_rating = '**&frac12' OR
+       my_star_rating = '***' OR
+       my_star_rating = '***&frac12' OR
+       my_star_rating = '****' OR
+       my_star_rating = 'N/A' OR
+       my_star_rating = '[not seen]');
 
 -- movie mpaa rating
 ALTER TABLE movie ADD CONSTRAINT mpaa_constraint
@@ -100,10 +100,10 @@ CHECK (mpaa = 'NR' OR
        mpaa = 'NC-17');
 
 -- listcontains rank
-ALTER TABLE listcontains ADD CONSTRAINT rank_constraint
-CHECK (rankinlist > 0);
+ALTER TABLE list_contains ADD CONSTRAINT rank_constraint
+CHECK (rank > 0);
 
 -- oscargivento status
-ALTER TABLE oscargivento ADD CONSTRAINT osc_stat_constraint
+ALTER TABLE oscar_given_to ADD CONSTRAINT osc_stat_constraint
 CHECK (status = 'won' OR
        status = 'nominated');
--- sql/db_tables.sql
+++ sql/db_tables.sql
@@ -4,56 +4,56 @@
 
 -- a movie entity
 CREATE TABLE movie (
-movieid serial NOT NULL,
+mid serial NOT NULL,
 title varchar(100) NOT NULL,
 year integer NOT NULL,
-mystarrating varchar(11) DEFAULT NULL,
+star_rating varchar(11) DEFAULT NULL,
 mpaa varchar(7) DEFAULT NULL,
 country varchar(30) DEFAULT NULL);
 
 -- a crewperson entity
-CREATE TABLE crewperson (
-crewid serial NOT NULL,
-lname varchar(20) NOT NULL,
-fname varchar(20) DEFAULT NULL,
-mname varchar(15) DEFAULT NULL);
+CREATE TABLE crew_person (
+cid serial NOT NULL,
+l_name varchar(20) NOT NULL,
+f_name varchar(20) DEFAULT NULL,
+m_name varchar(15) DEFAULT NULL);
 
 -- crewperson <--> movie relationship
-CREATE TABLE workedon (
-movieid integer NOT NULL,
-crewid integer NOT NULL,
+CREATE TABLE worked_on (
+mid integer NOT NULL,
+cid integer NOT NULL,
 position varhcar(20) NOT NULL);
 
 -- a genre entity
 CREATE TABLE genre (
-genname varchar(25) NOT NULL);
+gen_name varchar(25) NOT NULL);
 
 -- genre <--> movie relationship
-CREATE TABLE genrecontains (
-movieid integer NOT NULL,
-genname varchar(25) NOT NULL);
+CREATE TABLE genre_contains (
+mid integer NOT NULL,
+gen_name varchar(25) NOT NULL);
 
 -- an oscar entity
 CREATE TABLE oscar (
 category varchar(25) NOT NULL);
 
 -- oscar <--> movie relationship
-CREATE TABLE oscargivento (
-movieid integer NOT NULL,
+CREATE TABLE oscar_given_to (
+mid integer NOT NULL,
 category varchar(25) NOT NULL,
 recipient integer NOT NULL,       --(foreign key to crewperson.crewid)
 status varchar(10) DEFAULT NULL); --(nominated or won)
 
 -- a list entity
 CREATE TABLE list (
-listtitle varchar(50) NOT NULL,
-listauthor varchar(25) DEFAULT NULL);
+list_title varchar(50) NOT NULL,
+list_author varchar(25) DEFAULT NULL);
 
 -- list <--> movie relationship
-CREATE TABLE listcontains (
-movieid integer NOT NULL,
-listtitle varchar(50) NOT NULL,
-rankinlist integer DEFAULT NULL);
+CREATE TABLE list_contains (
+mid integer NOT NULL,
+list_title varchar(50) NOT NULL,
+rank integer DEFAULT NULL);
 
 -- a country entity
 --
@@ -63,4 +63,4 @@ rankinlist integer DEFAULT NULL);
 -- exists so that there doesn't have to be a big OR'd integrity contraint for a
 -- movie's country (like the star_contraint for the movie table).
 CREATE TABLE country (
-countryname varchar(30) NOT NULL);
+country_name varchar(30) NOT NULL);
