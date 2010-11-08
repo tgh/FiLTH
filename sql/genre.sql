4c640e4e77a46a739e39b08b0331eee6836c7002
Tyler Hayes <tgh@pdx.edu>
2010-11-08 10:59:45 -0800

    Add sql/genre.sql.

---
+++ sql/genre.sql
@@ -0,0 +1,15 @@
+INSERT INTO genre VALUES (DEFAULT, 'Drama');
+INSERT INTO genre VALUES (DEFAULT, 'Comedy');
+INSERT INTO genre VALUES (DEFAULT, 'Thriller');
+INSERT INTO genre VALUES (DEFAULT, 'Independent');
+INSERT INTO genre VALUES (DEFAULT, 'Fantasy');
+INSERT INTO genre VALUES (DEFAULT, 'Science-Fiction');
+INSERT INTO genre VALUES (DEFAULT, 'Animated');
+INSERT INTO genre VALUES (DEFAULT, 'Mystery');
+INSERT INTO genre VALUES (DEFAULT, 'Horror');
+INSERT INTO genre VALUES (DEFAULT, 'Action');
+INSERT INTO genre VALUES (DEFAULT, 'Adventure');
+INSERT INTO genre VALUES (DEFAULT, 'Documentary');
+INSERT INTO genre VALUES (DEFAULT, 'Christmas');
+INSERT INTO genre VALUES (DEFAULT, 'Short');
+INSERT INTO genre VALUES (DEFAULT, 'Unconventional');
