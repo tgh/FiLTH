85015c059c3d0bd0247ed7b167f885062bbf4b98
Tyler Hayes <tgh@pdx.edu>
2010-12-15 16:11:30 -0800

    Fix errors in init_database.sql.
    Added a space after "--" to all comments (for MySQL), added CREATE
    DATABASE filth; command, and fixed oscar_given_to FKEY declaration
    (was referecing oscar table not oscar_given_to).
    Tested with MySQL: works.

--- sql/init_database.sql
+++ sql/init_database.sql
@@ -1,9 +1,24 @@
------------------------
+/*
+ * This sql script is for Postgresql.  Also in the sql/ directory is a file
+ * named init_mysql_database.sql for MySQL.  The difference in these is the
+ * data types.  MySQL offers a tinyint--a numeric type of only one byte in size,
+ * which is great for a lot of these attributes.  However, Postgres only offers
+ * smallint, which is two bytes.
+ */
+
+-- ----------------------
+-- Create the database --
+-- ----------------------
+
+CREATE DATABASE filth;
+
+
+-- --------------------
 -- Create the tables --
------------------------
+-- --------------------
 
 -- a movie entity
-CREATE TABLE movie (
+CREATE TABLE filth.movie (
 mid serial NOT NULL,
 title varchar(100) NOT NULL,
 -- smallint is 2 bytes in Postgres, plenty of bits for a year
@@ -24,7 +39,7 @@ country smallint DEFAULT NULL);
 
 
 -- a crewperson entity
-CREATE TABLE crew_person (
+CREATE TABLE filth.crew_person (
 cid serial NOT NULL,
 l_name varchar(20) NOT NULL,
 f_name varchar(20) DEFAULT NULL,
@@ -32,48 +47,48 @@ m_name varchar(15) DEFAULT NULL);
 
 
 -- crewperson <--> movie relationship
-CREATE TABLE worked_on (
+CREATE TABLE filth.worked_on (
 mid smallint NOT NULL,
 cid smallint NOT NULL,
-position varhcar(20) NOT NULL);
+position varchar(20) NOT NULL);
 
 
 -- a genre entity
-CREATE TABLE genre (
+CREATE TABLE filth.genre (
 gid serial NOT NULL,
 gen_name varchar(25) NOT NULL);
 
 
 -- genre <--> movie relationship
-CREATE TABLE genre_contains (
+CREATE TABLE filth.genre_contains (
 mid smallint NOT NULL,
 gid smallint NOT NULL);
 
 
 -- an oscar entity
-CREATE TABLE oscar (
+CREATE TABLE filth.oscar (
 oid serial NOT NULL,
 category varchar(25) NOT NULL);
 
 
 -- oscar <--> movie relationship
-CREATE TABLE oscar_given_to (
+CREATE TABLE filth.oscar_given_to (
 mid smallint NOT NULL,
 oid smallint NOT NULL,
 cid smallint NOT NULL,
---status of the oscar: 0, 1, or 2 (nominated, won, or tie, respectively)
+-- status of the oscar: 0, 1, or 2 (nominated, won, or tie, respectively)
 status smallint DEFAULT NULL);
 
 
 -- a list entity
-CREATE TABLE list (
+CREATE TABLE filth.list (
 lid serial NOT NULL,
 list_title varchar(50) NOT NULL,
 list_author varchar(25) DEFAULT NULL);
 
 
 -- list <--> movie relationship
-CREATE TABLE list_contains (
+CREATE TABLE filth.list_contains (
 mid smallint NOT NULL,
 lid smallint NOT NULL,
 rank smallint DEFAULT NULL);
@@ -86,92 +101,92 @@ rank smallint DEFAULT NULL);
 -- multiple countries associated with it (I don't care), but as of now this only
 -- exists so that there doesn't have to be a big integrity contraint for a
 -- movie's country (like the star_contraint for the movie table).
-CREATE TABLE country (
+CREATE TABLE filth.country (
 coid serial NOT NULL,
 country_name varchar(30) NOT NULL);
 
 
------------------------------
+-- --------------------------
 -- Primary Key constraints --
------------------------------
+-- --------------------------
 
-ALTER TABLE movie ADD CONSTRAINT movie_pkey PRIMARY KEY(mid);
-ALTER TABLE crew_person ADD CONSTRAINT crew_pkey PRIMARY KEY(cid);
-ALTER TABLE worked_on ADD CONSTRAINT worked_pkey PRIMARY KEY(mid, cid, position);
-ALTER TABLE genre ADD CONSTRAINT genre_pkey PRIMARY KEY(gid);
-ALTER TABLE genre_contains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(mid, gid);
-ALTER TABLE oscar ADD CONSTRAINT oscar_pkey PRIMARY KEY (oid);
-ALTER TABLE oscar_given_to ADD CONSTRAINT oscargiven_pkey PRIMARY KEY(mid, oid);
-ALTER TABLE list ADD CONSTRAINT list_pkey PRIMARY KEY(lid);
-ALTER TABLE list_contains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(mid, lid);
-ALTER TABLE country ADD CONSTRAINT country_pkey PRIMARY KEY(coid);
+ALTER TABLE filth.movie ADD CONSTRAINT movie_pkey PRIMARY KEY(mid);
+ALTER TABLE filth.crew_person ADD CONSTRAINT crew_pkey PRIMARY KEY(cid);
+ALTER TABLE filth.worked_on ADD CONSTRAINT worked_pkey PRIMARY KEY(mid, cid, position);
+ALTER TABLE filth.genre ADD CONSTRAINT genre_pkey PRIMARY KEY(gid);
+ALTER TABLE filth.genre_contains ADD CONSTRAINT genrecontains_pkey PRIMARY KEY(mid, gid);
+ALTER TABLE filth.oscar ADD CONSTRAINT oscar_pkey PRIMARY KEY (oid);
+ALTER TABLE filth.oscar_given_to ADD CONSTRAINT oscargiven_pkey PRIMARY KEY(mid, oid);
+ALTER TABLE filth.list ADD CONSTRAINT list_pkey PRIMARY KEY(lid);
+ALTER TABLE filth.list_contains ADD CONSTRAINT listcontains_pkey PRIMARY KEY(mid, lid);
+ALTER TABLE filth.country ADD CONSTRAINT country_pkey PRIMARY KEY(coid);
 
 
------------------------------
+-- --------------------------
 -- Foreign Key constraints --
------------------------------
+-- --------------------------
 
 -- movie table country FK
-ALTER TABLE movie ADD CONSTRAINT movie_country_fkey
-FOREIGN KEY (country) REFERENCES country(coid)
+ALTER TABLE filth.movie ADD CONSTRAINT movie_country_fkey
+FOREIGN KEY (country) REFERENCES filth.country(coid)
 ON UPDATE CASCADE ON DELETE SET NULL;
 
 -- worked_on table movie FK
-ALTER TABLE worked_on ADD CONSTRAINT worked_mid_fkey
-FOREIGN KEY (mid) REFERENCES movie(mid)
+ALTER TABLE filth.worked_on ADD CONSTRAINT worked_mid_fkey
+FOREIGN KEY (mid) REFERENCES filth.movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- worked_on table crew FK
-ALTER TABLE worked_on ADD CONSTRAINT worked_cid_fkey
-FOREIGN KEY (cid) REFERENCES crew_person(cid)
+ALTER TABLE filth.worked_on ADD CONSTRAINT worked_cid_fkey
+FOREIGN KEY (cid) REFERENCES filth.crew_person(cid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- genre_contains table movie FK
-ALTER TABLE genre_contains ADD CONSTRAINT genre_mid_fkey
-FOREIGN KEY (mid) REFERENCES movie(mid)
+ALTER TABLE filth.genre_contains ADD CONSTRAINT genre_mid_fkey
+FOREIGN KEY (mid) REFERENCES filth.movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- genre_contains table genre FK
-ALTER TABLE genre_contains ADD CONSTRAINT genre_gid_fkey
-FOREIGN KEY (gid) REFERENCES genre(gid)
+ALTER TABLE filth.genre_contains ADD CONSTRAINT genre_gid_fkey
+FOREIGN KEY (gid) REFERENCES filth.genre(gid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscar_given_to table movie FK
-ALTER TABLE oscar ADD CONSTRAINT oscar_mid_fkey
-FOREIGN KEY (mid) REFERENCES movie(mid)
+ALTER TABLE filth.oscar_given_to ADD CONSTRAINT oscar_mid_fkey
+FOREIGN KEY (mid) REFERENCES filth.movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscar_given_to table crew FK
-ALTER TABLE oscar ADD CONSTRAINT oscar_cid_fkey
-FOREIGN KEY (cid) REFERENCES crew_person(cid)
+ALTER TABLE filth.oscar_given_to ADD CONSTRAINT oscar_cid_fkey
+FOREIGN KEY (cid) REFERENCES filth.crew_person(cid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- oscar_given_to table category FK
-ALTER TABLE oscar ADD CONSTRAINT oscar_oid_fkey
-FOREIGN KEY (oid) REFERENCES oscar(oid)
+ALTER TABLE filth.oscar_given_to ADD CONSTRAINT oscar_oid_fkey
+FOREIGN KEY (oid) REFERENCES filth.oscar(oid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- list_contains table movie FK
-ALTER TABLE list_contains ADD CONSTRAINT list_contains_movie_fkey
-FOREIGN KEY (mid) REFERENCES movie(mid)
+ALTER TABLE filth.list_contains ADD CONSTRAINT list_contains_movie_fkey
+FOREIGN KEY (mid) REFERENCES filth.movie(mid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- list_contains table list FK
-ALTER TABLE list_contains ADD CONSTRAINT list_contains_lid_fkey
-FOREIGN KEY (lid) REFERENCES list(lid)
+ALTER TABLE filth.list_contains ADD CONSTRAINT list_contains_lid_fkey
+FOREIGN KEY (lid) REFERENCES filth.list(lid)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 
----------------------------
+-- ------------------------
 -- Integrity constraints --
----------------------------
+-- ------------------------
 
 -- movie year
-ALTER TABLE movie ADD CONSTRAINT year_constraint
+ALTER TABLE filth.movie ADD CONSTRAINT year_constraint
 CHECK (year >= 1900 AND year <= 2012);
 
 -- movie star rating
-ALTER TABLE movie ADD CONSTRAINT star_constraint
+ALTER TABLE filth.movie ADD CONSTRAINT star_constraint
 CHECK (star_rating >= 0 AND star_rating <= 10);
 --  0 = no stars
 --  1 = ½*
@@ -186,15 +201,15 @@ CHECK (star_rating >= 0 AND star_rating <= 10);
 -- 10 = not seen
 
 -- movie mpaa rating
-ALTER TABLE movie ADD CONSTRAINT mpaa_constraint
+ALTER TABLE filth.movie ADD CONSTRAINT mpaa_constraint
 CHECK (mpaa IN ('NR', 'G', 'PG', 'PG-13', 'R', 'X', 'NC-17'));
 
 -- listcontains rank
-ALTER TABLE list_contains ADD CONSTRAINT rank_constraint
+ALTER TABLE filth.list_contains ADD CONSTRAINT rank_constraint
 CHECK (rank > 0);
 
 -- oscargivento status
-ALTER TABLE oscar_given_to ADD CONSTRAINT oscar_status_constraint
+ALTER TABLE filth.oscar_given_to ADD CONSTRAINT oscar_status_constraint
 CHECK (status >= 0 AND status <= 2);
 -- 0 = nominated
 -- 1 = won
