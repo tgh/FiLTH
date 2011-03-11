793e325178dfe2b5ccd72d0b711857674ecfe440
Tyler Hayes <tgh@pdx.edu>
2011-03-11 14:40:38 -0800

    Modify drop_and_create scripts to output to temp files
    Each psql command is redirected to a temp file in temp/

--- scripts/drop_and_create_filth_db.sh
+++ scripts/drop_and_create_filth_db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 filth_sql_path=~/Projects/FiLTH/sql
+filth_temp_path=~/Projects/FiLTH/temp
 
-psql -U postgres -c "DROP DATABASE filth;"
-createdb -U postgres -O postgres filth
-psql -U postgres -d filth -f $filth_sql_path/init_pg_database.sql
-psql -U postgres -d filth -f $filth_sql_path/genre.sql
-psql -U postgres -d filth -f $filth_sql_path/oscar.sql
-psql -U postgres -d filth -f $filth_sql_path/tyler.sql
-psql -U postgres -d filth -f $filth_sql_path/crewperson.sql
-psql -U postgres -d filth -f $filth_sql_path/movie.sql
+psql -U postgres -c "DROP DATABASE filth;" > $filth_temp_path/drop_filth_db_output.txt
+createdb -U postgres -O postgres filth >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/init_pg_database.sql >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/genre.sql >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/oscar.sql >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/tyler.sql >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/crewperson.sql >> $filth_temp_path/drop_filth_db_output.txt
+psql -U postgres -d filth -f $filth_sql_path/movie.sql >> $filth_temp_path/drop_filth_db_output.txt
--- scripts/drop_and_create_test_db.sh
+++ scripts/drop_and_create_test_db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 filth_sql_path=~/Projects/FiLTH/sql
+filth_temp_path=~/Projects/FiLTH/temp
 
-psql -U postgres -c "DROP DATABASE test;"
-createdb -U postgres -O postgres test
-psql -U postgres -d test -f $filth_sql_path/init_pg_database.sql
-psql -U postgres -d test -f $filth_sql_path/genre.sql
-psql -U postgres -d test -f $filth_sql_path/oscar.sql
-psql -U postgres -d test -f $filth_sql_path/tyler.sql
-psql -U postgres -d test -f $filth_sql_path/crewperson.sql
-psql -U postgres -d test -f $filth_sql_path/movie.sql
+psql -U postgres -c "DROP DATABASE test;" > $filth_temp_path/drop_test_db_output.txt
+createdb -U postgres -O postgres test >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/init_pg_database.sql >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/genre.sql >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/oscar.sql >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/tyler.sql >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/crewperson.sql >> $filth_temp_path/drop_test_db_output.txt
+psql -U postgres -d test -f $filth_sql_path/movie.sql >> $filth_temp_path/drop_test_db_output.txt
