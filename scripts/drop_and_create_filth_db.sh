b06b453e07b65b1eadc631fce735edafd7133729
Tyler Hayes <tgh@pdx.edu>
2011-03-11 18:03:54 -0800

    Add echoes to drop_ scripts & fix redirect of output
    Closes tciket #45

--- scripts/drop_and_create_filth_db.sh
+++ scripts/drop_and_create_filth_db.sh
@@ -3,11 +3,34 @@
 filth_sql_path=~/Projects/FiLTH/sql
 filth_temp_path=~/Projects/FiLTH/temp
 
-psql -U postgres -c "DROP DATABASE filth;" > $filth_temp_path/drop_filth_db_output.txt
-createdb -U postgres -O postgres filth >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/init_pg_database.sql >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/genre.sql >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/oscar.sql >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/tyler.sql >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/crewperson.sql >> $filth_temp_path/drop_filth_db_output.txt
-psql -U postgres -d filth -f $filth_sql_path/movie.sql >> $filth_temp_path/drop_filth_db_output.txt
+echo "Dropping filth database..."
+sleep 0.5
+psql -U postgres -c "DROP DATABASE filth;" > /dev/null 2>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Creating database filth..."
+sleep 0.5
+createdb -U postgres -O postgres filth > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Creating database schema..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/init_pg_database.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Populating genre table..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/genre.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Populating oscar table..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/oscar.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Populating tyler table..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/tyler.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Populating crewperson table..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/crewperson.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
+
+echo "Populating movie table..."
+sleep 0.5
+psql -U postgres -d filth -f $filth_sql_path/movie.sql > /dev/null 2>>$filth_temp_path/drop_filth_db_error.txt
--- scripts/drop_and_create_test_db.sh
+++ scripts/drop_and_create_test_db.sh
@@ -3,11 +3,34 @@
 filth_sql_path=~/Projects/FiLTH/sql
 filth_temp_path=~/Projects/FiLTH/temp
 
-psql -U postgres -c "DROP DATABASE test;" > $filth_temp_path/drop_test_db_output.txt
-createdb -U postgres -O postgres test >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/init_pg_database.sql >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/genre.sql >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/oscar.sql >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/tyler.sql >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/crewperson.sql >> $filth_temp_path/drop_test_db_output.txt
-psql -U postgres -d test -f $filth_sql_path/movie.sql >> $filth_temp_path/drop_test_db_output.txt
+echo "Dropping test database..."
+sleep 0.5
+psql -U postgres -c "DROP DATABASE test;" > /dev/null 2>$filth_temp_path/drop_test_db_error.txt
+
+echo "Creating database test..."
+sleep 0.5
+createdb -U postgres -O postgres test > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Creating database schema..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/init_pg_database.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Populating genre table..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/genre.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Populating oscar table..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/oscar.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Populating tyler table..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/tyler.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Populating crewperson table..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/crewperson.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
+
+echo "Populating movie table..."
+sleep 0.5
+psql -U postgres -d test -f $filth_sql_path/movie.sql > /dev/null 2>>$filth_temp_path/drop_test_db_error.txt
