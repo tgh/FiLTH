8cbccc8c46765fdd04db9265effb2b6372a0fd45
Tyler Hayes <tgh@pdx.edu>
2011-03-14 19:52:26 -0700

    Add runop.sh to run OscarParser

---
+++ scripts/runop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+filth_path=~/Projects/FiLTH
+
+# run the OscarParser java program
+cd $filth_path/bin/
+java -cp $filth_path/bin/:$filth_path/jar/postgresql-8.4-701.jdbc4.jar:$filth_path/jar/tylerhayes.tools.jar:$filth_path/jar/javacsv.jar OscarParser > $filth_path/sql/oscar_given_to.sql
+cd $filth_path/scripts/
