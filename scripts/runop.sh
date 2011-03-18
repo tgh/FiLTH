b41df0ca410571ff378481846d3ca8bd7ecb4adc
Tyler Hayes <tgh@pdx.edu>
2011-03-18 13:54:31 -0700

    Remove redirect stdout in runop.sh
    since OscarParser writes directly to oscar_given_to.sql now.

--- scripts/runop.sh
+++ scripts/runop.sh
@@ -4,5 +4,5 @@ filth_path=~/Projects/FiLTH
 
 # run the OscarParser java program
 cd $filth_path/bin/
-java -cp $filth_path/bin/:$filth_path/jar/postgresql-8.4-701.jdbc4.jar:$filth_path/jar/tylerhayes.tools.jar:$filth_path/jar/javacsv.jar OscarParser > $filth_path/sql/oscar_given_to.sql
+java -cp $filth_path/bin/:$filth_path/jar/postgresql-8.4-701.jdbc4.jar:$filth_path/jar/tylerhayes.tools.jar:$filth_path/jar/javacsv.jar OscarParser
 cd $filth_path/scripts/
