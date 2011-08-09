58466dff6d8961db14b4749cd716699905cb2981
Tyler Hayes <tgh@pdx.edu>
2011-08-08 20:42:24 -0700

    Add scripts/update_movie_table.sh
    updates movie table without having to destroy and create entire
    database

---
+++ scripts/update_movie_table.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+##
+# This script updates only the movie table of the database.
+#
+
+filth_path=~/Projects/FiLTH
+filth_temp_path=~/Projects/FiLTH/temp
+
+# remove the previous_movie_ratings.txt file that is created and checked for by movie.sh
+echo "Removing previous_movie_ratings.txt (if exists)..."
+sleep 0.5
+if [ -f $filth_temp_path/previous_movie_ratings.txt ]
+then
+  rm $filth_temp_path/previous_movie_ratings.txt
+fi
+
+# create the /sql/movie.sql file based on /data/Movie_Ratings.doc
+echo "Running movie.sh script..."
+$filth_path/scripts/movie.sh
+
+# remove all tuples from the movie table in the database
+echo "Removing all rows in movie table..."
+sleep 0.5
+psql -U postgres -d filth -c "DELETE FROM movie;" > /dev/null 2>$filth_temp_path/update_movie_error.txt
+
+# reset the movie id primary key sequence
+echo "Resetting the mid sequence for the movie table..."
+sleep 0.5
+psql -U postgres -d filth -c "ALTER SEQUENCE movie_mid_seq RESTART WITH 1;" > /dev/null 2>>$filth_temp_path/update_movie_error.txt
+
+# execute SQL statements in movie.sql
+echo "Inserting all movies in movie.sql..."
+psql -U postgres -d filth -f $filth_path/sql/movie.sql > /dev/null 2>>$filth_temp_path/update_movie_error.txt
