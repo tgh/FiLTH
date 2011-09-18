9fa11b58716814965e56744df4f639a6fbb987b2
Tyler Hayes <tylerhayes80@gmail.com>
2011-09-18 10:48:50 -0700

    Add 2 movies
    Mona Lisa Smile
    It Might Get Loud

--- sql/movie.sql
+++ sql/movie.sql
@@ -1814,3 +1814,5 @@ INSERT INTO movie VALUES (DEFAULT, 'The Battle of Algiers', 1968, NULL, NULL, NU
 INSERT INTO movie VALUES (DEFAULT, 'Mr. Hulot''s Holiday', 1955, NULL, NULL, NULL, NULL);
 INSERT INTO movie VALUES (DEFAULT, 'Wild Strawberries', 1959, NULL, NULL, NULL, NULL);
 INSERT INTO movie VALUES (DEFAULT, 'My Life as a Dog', 1987, NULL, NULL, NULL, NULL);
+INSERT INTO movie VALUES (DEFAULT, 'It Might Get Loud', 2009, 5, 2, 'USA', NULL);
+INSERT INTO movie VALUES (DEFAULT, 'Mona Lisa Smile', 2003, 6, 3, 'USA', NULL);
