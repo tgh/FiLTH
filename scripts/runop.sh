288efb4ea2f43ac9fd82a38b628adfdd9e5801de
Tyler Hayes <tylerhayes80@gmail.com>
2011-10-28 19:39:37 -0700

    Add combine movie and crew_person sql files to runop.sh

--- scripts/runop.sh
+++ scripts/runop.sh
@@ -20,3 +20,18 @@ fi
 cd $filth_path/bin/
 java -cp $filth_path/bin/:$filth_path/jar/postgresql-8.4-701.jdbc4.jar:$filth_path/jar/tylerhayes.tools.jar:$filth_path/jar/javacsv.jar OscarParser $1 $2
 cd $filth_path/scripts/
+
+# combine the created movie sql file (if created) with movie.sql
+if [ -e $filth_path/sql/movie2.sql ]
+then
+  cat $filth_path/sql/movie2.sql >> $filth_path/sql/movie.sql
+  rm $filth_path/sql/movie2.sql
+fi
+
+# combine the created crew_person sql file (if created) with crew_person.sql
+if [ -e $filth_path/sql/crew_person2.sql ]
+then
+  cat $filth_path/sql/crew_person2.sql >> $filth_path/sql/crew_person.sql
+  rm $filth_path/sql/crew_person2.sql
+fi
+
