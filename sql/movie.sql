cd70f47688f907834635acb43b79d1185387e91d
Tyler Hayes <tylerhayes80@gmail.com>
2011-12-02 11:10:19 -0800

    Add 'Wall Street: Money Never Sleeps'

--- sql/movie.sql
+++ sql/movie.sql
@@ -3334,3 +3334,4 @@ INSERT INTO movie VALUES (DEFAULT, 'J. Edgar', 2011, '**½', 'PG-13', 'USA', NUL
 INSERT INTO movie VALUES (DEFAULT, 'Rise of the Planet of the Apes', 2011, '**½', 'PG-13', 'USA', NULL);
 INSERT INTO movie VALUES (DEFAULT, 'Arthur Christmas', 2011, '***', 'PG', 'England', NULL);
 INSERT INTO movie VALUES (DEFAULT, 'Trust', 2011, '***½', 'R', 'USA', NULL);
+INSERT INTO movie VALUES (DEFAULT, 'Wall Street: Money Never Sleeps', 2010, '**½', 'PG-13', 'USA', NULL);
