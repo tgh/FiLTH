0939fb9eafc55912c7e6ffa54a74a8e6e6d453c9
Tyler Hayes <tylerhayes80@gmail.com>
2012-08-23 18:24:07 -0400

    Add Duane Hopwood

--- sql/movie.sql
+++ sql/movie.sql
@@ -3402,3 +3402,4 @@ INSERT INTO movie VALUES (DEFAULT, 'The Dark Knight Rises', 2012, '***½', 'PG-1
 INSERT INTO movie VALUES (DEFAULT, 'Mr. Popper''''s Penguins', 2011, '**', 'PG', 'USA', NULL);
 INSERT INTO movie VALUES (DEFAULT, 'Never Let Me Go', 2010, '***', 'R', 'England', NULL);
 INSERT INTO movie VALUES (DEFAULT, 'Hope Springs', 2012, '***', 'PG-13', 'USA', NULL);
+INSERT INTO movie VALUES (DEFAULT, 'Duane Hopwood', 2005, '***', 'R', 'USA', NULL);
