0d67cc5966ea8b29408b1961f00aa3e4c6bb8727
Tyler Hayes <tylerhayes80@gmail.com>
2012-08-23 21:07:03 -0400

    Fix typo in crew2sql.py

--- scripts/crew2sql.py
+++ scripts/crew2sql.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
   # oscar_given_to have a foreign key to the primary key of crew_person (cid)
   # which can't be NULL, but there are many records in oscar_given_to where a
   # NULL value is desired (for oscars in which recipients don't matter like
-  # Best Picture, Best Documentary, ect).  The sequence giving the values of
+  # Best Picture, Best Documentary, etc).  The sequence giving the values of
   # cid has been set to start at -1 by the database schema (see
   # sql/init_pg_database.sql).  This -1 value is to represent a "no-recipient"
   # value--a work around for not be able to use NULL.  The first inserted record
