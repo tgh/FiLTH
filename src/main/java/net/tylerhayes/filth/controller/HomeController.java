0655d64ee1852736f9b08e4a1565fc08ff2e4f58
Tyler Hayes <tylerhayes80@gmail.com>
2012-09-04 21:43:32 -0400

    Add ModelMap to HomeController

--- src/main/java/net/tylerhayes/filth/controller/HomeController.java
+++ src/main/java/net/tylerhayes/filth/controller/HomeController.java
@@ -1,6 +1,7 @@
 package net.tylerhayes.filth.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,8 +13,15 @@ public class HomeController {
     public static final String HOME_URL = "/home";
   }
   
+  public static final class ModelKeys {
+    public static final String HELLO = "hello";
+  }
+  
   @RequestMapping(value = {URLPrefix.HOME, URLPrefix.HOME_URL}, method = RequestMethod.GET)
-  public String home() {
-    return "Home";
+  public ModelMap home() {
+    ModelMap mm = new ModelMap();
+    mm.addAttribute(ModelKeys.HELLO, "Hello");
+    
+    return mm;
   }
 }
