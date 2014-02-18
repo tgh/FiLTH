dc544ea9708dc9a6bab5318368548716aab13a26
Tyler Hayes <tylerhayes80@gmail.com>
2014-02-18 18:35:28 -0500

    Add positions: Producer, Narrator, Character Voice

--- sql/position.sql
+++ sql/position.sql
@@ -3,3 +3,6 @@ INSERT INTO position VALUES('Actress');
 INSERT INTO position VALUES('Cinematographer');
 INSERT INTO position VALUES('Director');
 INSERT INTO position VALUES('Screenwriter');
+INSERT INTO position VALUES('Producer');
+INSERT INTO position VALUES('Character Voice');
+INSERT INTO position VALUES('Narrator');
