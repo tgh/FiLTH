06f733a22592b23e97b87455734011c85112536e
Tyler Hayes <tylerhayes80@gmail.com>
2014-11-04 21:45:45 -0500

    Add crew and tags

---
+++ sql/crew_person_additions.sql
@@ -0,0 +1,3 @@
+INSERT INTO crew_person VALUES (2239, 'Emmerich', 'Roland', NULL, 'Director');  -- Director: Roland Emmerich
+INSERT INTO crew_person VALUES (2240, 'Kloser', 'Harald', NULL, 'Screenwriter');  -- Screenwriter: Harald Kloser
+INSERT INTO crew_person VALUES (2241, 'Watson', 'Ian', NULL, 'Screenwriter');  -- Screenwriter: Ian Watson
