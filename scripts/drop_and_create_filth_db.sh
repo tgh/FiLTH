d95c20803e31132860b3f6ad73584e74eb8b479e
Tyler Hayes <thayes@amplify.com>
2015-06-04 14:41:47 -0400

    Use user 'filth_admin' in drop_and_create... script

--- scripts/drop_and_create_filth_db.sh
+++ scripts/drop_and_create_filth_db.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script assumes:
+#  - a postgresql user "filth_admin" has been created (see the "SETTING UP ENVIRONMENT"
+#    section in /doc/NOTES about setting up the user
+#  - the OS user "postgres" is running the script
+
 source common.sh
 
 ERROR_FILE=$FILTH_TEMP_PATH/drop_filth_db_error.txt
@@ -8,7 +13,7 @@ ERROR_FILE=$FILTH_TEMP_PATH/drop_filth_db_error.txt
 function populate_db_table {
   echo "Populating $1..."
   sleep 0.5
-  psql -U postgres -d filth -f $FILTH_SQL_PATH/$1.sql > /dev/null 2>>$ERROR_FILE
+  psql -U filth_admin -d filth -f $FILTH_SQL_PATH/$1.sql > /dev/null 2>>$ERROR_FILE
 }
 
 
@@ -17,17 +22,17 @@ function populate_db_table {
 
 echo "Dropping filth database..."
 sleep 0.5
-psql -U postgres -c "DROP DATABASE filth;" > /dev/null 2>$ERROR_FILE
+psql -U filth_admin -c "DROP DATABASE filth;" > /dev/null 2>$ERROR_FILE
 
 
 echo "Creating database filth..."
 sleep 0.5
-createdb -U postgres -O postgres filth > /dev/null 2>>$ERROR_FILE
+createdb -U filth_admin -O filth_admin filth > /dev/null 2>>$ERROR_FILE
 
 
 echo "Creating database schema..."
 sleep 0.5
-psql -U postgres -d filth -f $FILTH_SQL_PATH/init_pg_database.sql > /dev/null 2>>$ERROR_FILE
+psql -U filth_admin -d filth -f $FILTH_SQL_PATH/init_pg_database.sql > /dev/null 2>>$ERROR_FILE
 
 
 #populate entity tables
