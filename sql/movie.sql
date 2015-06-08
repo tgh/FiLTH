deba85ce4934649348c4ba31c56a056ff2d84910
Tyler Hayes <thayes@amplify.com>
2015-06-08 13:39:19 -0400

    Fix year of One Lucky Elephant

--- sql/movie.sql
+++ sql/movie.sql
@@ -3650,5 +3650,5 @@ INSERT INTO movie VALUES (3650, 'Spy', 2015, '**½', 'R', 'USA', NULL);
 INSERT INTO movie VALUES (3651, 'X-Men: First Class', 2011, '**½', 'PG-13', 'USA', NULL);
 INSERT INTO movie VALUES (3652, 'Haute Cuisine', 2012, '**½', 'PG-13', 'France', NULL);
 INSERT INTO movie VALUES (3653, 'The Man Who Shot Liberty Valance', 1962, '***', 'NR', 'USA', NULL);
-INSERT INTO movie VALUES (3654, 'One Lucky Elephant', 2010, '***', 'NR', 'USA', NULL);
+INSERT INTO movie VALUES (3654, 'One Lucky Elephant', 2011, '***', 'NR', 'USA', NULL);
 INSERT INTO movie VALUES (3655, 'Payback', 1999, '**½', 'R', 'USA', NULL);
--- sql/movie_additions.sql
+++ sql/movie_additions.sql
@@ -2,3 +2,4 @@ INSERT INTO movie VALUES (3652, 'Haute Cuisine', 2012, '**½', 'PG-13', 'France'
 INSERT INTO movie VALUES (3653, 'The Man Who Shot Liberty Valance', 1962, '***', 'NR', 'USA', NULL);
 INSERT INTO movie VALUES (3654, 'One Lucky Elephant', 2010, '***', 'NR', 'USA', NULL);
 INSERT INTO movie VALUES (3655, 'Payback', 1999, '**½', 'R', 'USA', NULL);
+UPDATE movie SET year = 2011 WHERE mid = 3654;
