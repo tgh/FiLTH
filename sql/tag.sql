3d170828d6f0af63bf798cc7c8f70cce33ad4074
Tyler Hayes <thayes@amplify.com>
2015-09-01 09:00:07 -0400

    Add more tags

--- sql/tag.sql
+++ sql/tag.sql
@@ -354,3 +354,5 @@ INSERT INTO tag VALUES (353, 'civil-rights', NULL);
 INSERT INTO tag VALUES (354, 'environmental', NULL);
 INSERT INTO tag VALUES (355, 'sociopath', 143);
 INSERT INTO tag VALUES (356, 'chess', NULL);
+INSERT INTO tag VALUES (357, 'magic/magician(s)', 61);
+INSERT INTO tag VALUES (358, 'college', NULL);
--- sql/tag_additions.sql
+++ sql/tag_additions.sql
@@ -2,3 +2,5 @@ INSERT INTO tag VALUES (353, 'civil-rights', NULL);
 INSERT INTO tag VALUES (354, 'environmental', NULL);
 INSERT INTO tag VALUES (355, 'sociopath', 143);
 INSERT INTO tag VALUES (356, 'chess', NULL);
+INSERT INTO tag VALUES (357, 'magic/magician(s)', NULL);
+INSERT INTO tag VALUES (358, 'college', NULL);
