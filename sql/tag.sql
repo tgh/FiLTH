eb1e8d916835f8a8170dc1b84d7a7f58a9c77f92
Tyler Hayes <thayes@amplify.com>
2015-10-01 13:53:43 -0400

    Add more tags

--- sql/tag.sql
+++ sql/tag.sql
@@ -360,3 +360,4 @@ INSERT INTO tag VALUES (359, 'golf', 20);
 INSERT INTO tag VALUES (360, 'missing-person(s)', NULL);
 INSERT INTO tag VALUES (361, 'non-professional-actor(s)', NULL);
 INSERT INTO tag VALUES (362, 'loss', NULL);
+INSERT INTO tag VALUES (363, 'deafness', 156);
