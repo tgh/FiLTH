cc553f791568bba15a41d9b54f6507aabf5fa6b3
Tyler Hayes <thayes@amplify.com>
2015-12-08 14:05:14 -0500

    Add more tags

--- sql/tag.sql
+++ sql/tag.sql
@@ -368,3 +368,4 @@ INSERT INTO filth.tag VALUES (367, 'insomnia', 156);
 INSERT INTO filth.tag VALUES (368, 'gulf-war', 17);
 INSERT INTO filth.tag VALUES (369, 'journalism/reporter(s)', NULL);
 INSERT INTO filth.tag VALUES (370, 'undercover-cop(s)', 134);
+INSERT INTO filth.tag VALUES (371, 'circus', 61);
--- sql/tag_additions.sql
+++ sql/tag_additions.sql
@@ -1,3 +1,4 @@
 INSERT INTO tag VALUES (368, 'gulf-war', 17);
 INSERT INTO tag VALUES (369, 'journalism/reporter(s)', NULL);
 INSERT INTO tag VALUES (370, 'undercover-cop(s)', 134);
+INSERT INTO filth.tag VALUES (371, 'circus', 61);
