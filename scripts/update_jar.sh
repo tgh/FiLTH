46d2a9c57d18ce0ddca7185803e48e67938271a4
Tyler Hayes <thayes@amplify.com>
2016-02-25 14:41:35 -0500

    Remove old and useless update_jar script

--- scripts/update_jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy the tylerhayes.tools.jar file from the JavaTools project
-cp ~/Projects/JavaTools/tylerhayes.tools.jar ~/workspace/FiLTH/jar/tylerhayes.tools/
-# create an empty file in the temp directory which the runop.sh script will
-#  check for.  runop.sh will stop execution if the file is there
-#  (indicating that OscarParser needs to be recompiled with this new jar
-#  before runop.sh can execute).  cop.sh will remove the file.
-touch ~/workspace/FiLTH/temp/recompile_OscarParser
