d85d1659f02fca89865ba991a3edcd4c62437266
Tyler Hayes <thayes@amplify.com>
2016-02-25 15:09:59 -0500

    Update drop_and_create... to update id sequences

--- scripts/drop_and_create_filth_db.sh
+++ scripts/drop_and_create_filth_db.sh
@@ -9,6 +9,22 @@ source common.sh
 LOG_FILE=$FILTH_PATH/logs/drop_and_create_filth_db.log
 
 
+function getLastFileId {
+  file=$1.sql
+  lastFileId=`tac $FILTH_SQL_PATH/$file | egrep -m 1 . | sed -E "s/.*\(([0-9]+),.*/\\1/"`
+}
+
+function update_sequence {
+  echo "Updating id sequence for $1..."
+  sleep 0.5
+  getLastFileId $1
+  table=$1
+  tableId="$(echo $table | head -c 1)id" # take the first letter of the table name and append "id"
+  sequenceValue=$lastFileId
+  sequence=${table}_${tableId}_seq
+  psql -U filth_admin -d filth -c "SELECT setval('filth.$sequence', $sequenceValue);" > /dev/null 2>>$LOG_FILE
+}
+
 function populate_db_table {
   echo "Populating $1..."
   sleep 0.5
@@ -34,46 +50,55 @@ if [ $# -gt 0 ]
     for i in $@
     do
       clear_db_table $1
-      populate_db_table $1
+  ### populate_db_table $1
     done
   #drop and create the entire database
   else
-    echo "Dropping existing filth db..."
-    sleep 0.5
-    sudo -u postgres dropdb --if-exists filth
+### echo "Dropping existing filth db..."
+### sleep 0.5
+### sudo -u postgres dropdb --if-exists filth
 
-    echo "Creating database filth..."
-    sleep 0.5
-    sudo -u postgres createdb --owner=filth_admin filth
+### echo "Creating database filth..."
+### sleep 0.5
+### sudo -u postgres createdb --owner=filth_admin filth
 
-    echo "Creating schema..."
-    sleep 0.5
-    psql -U filth_admin -d filth -f $FILTH_SQL_PATH/init_pg_database.sql > /dev/null 2>>$LOG_FILE
+### echo "Creating schema..."
+### sleep 0.5
+### psql -U filth_admin -d filth -f $FILTH_SQL_PATH/init_pg_database.sql > /dev/null 2>>$LOG_FILE
 
+
+    #populate integrity tables
+### populate_db_table "country"
+### populate_db_table "movie_link_type"
+### populate_db_table "mpaa"
+### populate_db_table "position"
+### populate_db_table "star_rating"
 
     #populate entity tables
-    populate_db_table "movie_link_type"
-    populate_db_table "country"
-    populate_db_table "oscar"
-    populate_db_table "tyler"
-    populate_db_table "mpaa"
-    populate_db_table "star_rating"
-    populate_db_table "position"
-    populate_db_table "crew_person"
-    populate_db_table "tag"
-    populate_db_table "movie"
-    populate_db_table "list"
+### populate_db_table "crew_person"
+### populate_db_table "list"
+### populate_db_table "movie"
+### populate_db_table "oscar"
+### populate_db_table "tag"
+### populate_db_table "tyler"
 
     #populate relationship tables
-    populate_db_table "movie_link"
-    populate_db_table "oscar_given_to"
-    populate_db_table "tag_given_to"
-    populate_db_table "worked_on"
-    populate_db_table "tyler_given_to"
-    populate_db_table "list_contains"
+### populate_db_table "list_contains"
+### populate_db_table "movie_link"
+### populate_db_table "oscar_given_to"
+### populate_db_table "tag_given_to"
+### populate_db_table "tyler_given_to"
+### populate_db_table "worked_on"
 
+    #update sequences
+    update_sequence "crew_person"
+    update_sequence "list"
+    update_sequence "movie"
+    update_sequence "oscar"
+    update_sequence "tag"
+    update_sequence "tyler"
 
-    echo "Creating temp/previous_movie_ratings.txt..."
-    sleep 0.5
-    antiword -w 120 $FILTH_PATH/data/Movie_Ratings.doc > $FILTH_TEMP_PATH/previous_movie_ratings.txt
+### echo "Creating temp/previous_movie_ratings.txt..."
+### sleep 0.5
+### antiword -w 120 $FILTH_PATH/data/Movie_Ratings.doc > $FILTH_TEMP_PATH/previous_movie_ratings.txt
 fi
