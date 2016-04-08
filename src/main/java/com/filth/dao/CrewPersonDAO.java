5f2aa646ae23cc4e276b0cf662aef41085065247
Tyler Hayes <thayes@amplify.com>
2016-04-08 08:56:54 -0400

    Override getAll in CrewPersonDAO to ignore dummy row

--- src/main/java/com/filth/dao/CrewPersonDAO.java
+++ src/main/java/com/filth/dao/CrewPersonDAO.java
@@ -1,5 +1,8 @@
 package com.filth.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import com.filth.model.CrewPerson;
@@ -10,6 +13,13 @@ public class CrewPersonDAO extends HibernateDAO<CrewPerson> {
     @Override
     protected Class<CrewPerson> getEntityClass() {
         return CrewPerson.class;
+    }
+    
+    @Override
+    public List<CrewPerson> getAll() {
+        //exclude the dummy crew whose id is 0
+        Query query = getSession().createQuery("from CrewPerson where cid > 0");
+        return extractTypedList(query);
     }
     
 }
