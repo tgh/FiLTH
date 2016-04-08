936e36f2862e9a9b66298bca8016cd6dbda9f2bf
Tyler Hayes <thayes@amplify.com>
2016-04-08 10:05:07 -0400

    Add ServiceTest and ServiceTestRunner
    Most likely need to be more fleshed out, however

---
+++ src/test/java/com/filth/service/ServiceTest.java
@@ -0,0 +1,35 @@
+package com.filth.service;
+
+import java.lang.reflect.Method;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Abstract class for all *ServiceTests
+ */
+@RunWith(ServiceTestRunner.class)
+@ContextConfiguration(locations = {"classpath*:/service-test-context.xml"})
+public abstract class ServiceTest {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceTest.class);
+
+    /**
+     * Gets called by ServiceTestRunner before every test method.
+     */
+    public final void setUpBeforeTestMethod(Method method) {
+        LOGGER.info("Setting up prior to running test: " + method.getName());
+        
+//        // Set up session and request scoping
+//        _applicationContextService.setUpMockSessionAndScoping();
+//        if (!method.isAnnotationPresent(KeepNonStaticTables.class)) {
+//            _dbTruncator.truncateNonStaticTables();
+//        }
+//        _dbDataLoader.runDataMakers(method);
+//        _mockUserService.setCurrentSecureUser(null);
+//        _mockHttpSession = new MockHttpSession();
+//        getNewRequest();
+    }
+}
---
+++ src/test/java/com/filth/service/ServiceTestRunner.java
@@ -0,0 +1,50 @@
+package com.filth.service;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.FrameworkMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Custom test-runner so we can perform any necessary logic prior to test
+ * execution (such as db setup, for example).
+ */
+public class ServiceTestRunner extends SpringJUnit4ClassRunner {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceTestRunner.class);
+    
+    private ServiceTest _test;
+
+    public ServiceTestRunner(Class<?> clazz) throws Exception {
+        super(clazz);
+        _test = (ServiceTest) createTest();
+    }
+    
+    private boolean isTestRunnable(Method method) {
+        String methodToRun = System.getProperty("net.wgen.spring.common.test.method");
+        LOGGER.debug("Method to run: " + methodToRun);
+        if(StringUtils.isBlank(methodToRun)) {
+            // if a specific method isn't requested, every method is runnable.
+            return true;    
+        }
+        // if a specific method is requested, run only the requested method
+        return method.getName().equals(methodToRun);
+    }
+    
+    @Override
+    protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+        if (isTestRunnable(method.getMethod())) {
+            _test.setUpBeforeTestMethod(method.getMethod());
+            super.runChild(new FrameworkMethod(method.getMethod()), notifier);
+        } else {
+            notifier.fireTestIgnored(Description.createTestDescription(
+                    getTestClass().getJavaClass(), method.getName()));
+        }
+    }
+
+}
