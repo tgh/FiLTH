7a0d074605e51b5f1e2a646e38e5ee799210c503
Tyler Hayes <thayes@amplify.com>
2017-02-03 14:16:39 -0800

    Add movie sequences, collections, and links
    - lord of the rings
    - harry potter
    - paradise lost/west of memphis docs

--- sql/movie_sequence.sql
+++ sql/movie_sequence.sql
@@ -2,3 +2,6 @@ INSERT INTO filth.movie_sequence VALUES(1, 'The ''Up'' Documentaries', 'SERIES')
 INSERT INTO filth.movie_sequence VALUES(2, 'The Star Wars Saga', 'SAGA');
 INSERT INTO filth.movie_sequence VALUES(3, 'The Star Wars Trilogy', 'TRILOGY');
 INSERT INTO filth.movie_sequence VALUES(4, 'The Star Wars Prequel Trilogy', 'TRILOGY');
+INSERT INTO filth.movie_sequence VALUES(5, 'The Harry Potter Series', 'SERIES');
+INSERT INTO filth.movie_sequence VALUES(6, 'The Lord of the Rings Trilogy', 'TRILOGY');
+INSERT INTO filth.movie_sequence VALUES(7, 'The Paradise Lost Documentary Trilogy', 'TRILOGY');
--- sql/movie_sequence_movie.sql
+++ sql/movie_sequence_movie.sql
@@ -19,3 +19,17 @@ INSERT INTO filth.movie_sequence_movie VALUES(18, 3, 1550, 3); -- Star Wars: Epi
 INSERT INTO filth.movie_sequence_movie VALUES(19, 4, 1545, 1); -- Star Wars: Episode I - The Phantom Menace (1999) in sequence The Star Wars Prequel Trilogy
 INSERT INTO filth.movie_sequence_movie VALUES(20, 4, 1546, 2); -- Star Wars: Episode II - Attack of the Clones (2002) in sequence The Star Wars Prequel Trilogy
 INSERT INTO filth.movie_sequence_movie VALUES(21, 4, 1547, 3); -- Star Wars: Episode III - Revenge of the Sith (2005) in sequence The Star Wars Prequel Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(22, 5, 717, 1); -- Harry Potter and the Sorcerer's Stone (2001) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(23, 5, 718, 2); -- Harry Potter and the Chamber of Secrets (2002) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(24, 5, 719, 3); -- Harry Potter and the Prisoner of Azkaban (2004) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(25, 5, 720, 4); -- Harry Potter and the Goblet of Fire (2005) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(26, 5, 721, 5); -- Harry Potter and the Order of the Phoenix (2007) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(27, 5, 722, 6); -- Harry Potter and the Half-Blood Prince (2009) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(28, 5, 723, 7); -- Harry Potter and the Deathly Hallows Part 1 (2010) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(29, 5, 724, 8); -- Harry Potter and the Deathly Hallows Part 2 (2011) in sequence The Harry Potter Series
+INSERT INTO filth.movie_sequence_movie VALUES(30, 6, 977, 1); -- The Lord of the Rings: The Fellowship of the Rings (2001) in sequence The Lord of the Rings Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(31, 6, 978, 2); -- The Lord of the Rings: The Two Towers (2002) in sequence The Lord of the Rings Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(32, 6, 979, 3); -- The Lord of the Rings: The Return of the King (20013 in sequence The Lord of the Rings Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(33, 7, 1235, 1); -- Paradise Lost: Child Murders at Robin Hood Hills (1996) in sequence The Paradise Lost Documentary Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(34, 7, 1236, 2); -- Paradise Lost 2: Revelations (2000) in sequence The Paradise Lost Documentary Trilogy
+INSERT INTO filth.movie_sequence_movie VALUES(35, 7, 3412, 3); -- Paradise Lost 3: Purgatory (2011) in sequence The Paradise Lost Documentary Trilogy
