d4e05ade701d65091d24c1e31d93f5a4688e79bb
Tyler Hayes <thayes@amplify.com>
2018-01-05 16:35:34 -0800

    Add Bourne Legacy

--- sql/movie.sql
+++ sql/movie.sql
@@ -4276,3 +4276,4 @@ INSERT INTO filth.movie VALUES (4280, 'A Christmas Carol', 1984, '***', 'PG', 'E
 INSERT INTO filth.movie VALUES (4281, 'Logan Lucky', 2017, '***', 'PG-13', 'USA', NULL, 'tt5439796', 0, 399170, NULL, NULL, 118);
 INSERT INTO filth.movie VALUES (4282, 'Baby Driver', 2017, '**½', 'R', 'England', 'There were some flat characters, screenplay contrivances, and the ending was over-the-top and too Hollywood. The opening sequence is excellent though and the music incorporated into the film is fun (gun shots firing along with the music, for example).', 'tt3890160', 0, 339403, NULL, NULL, 112);
 INSERT INTO filth.movie VALUES (4283, 'Haywire', 2011, '***', 'R', 'USA', 'Really realistic fight scenes--well done.', 'tt1506999', 0, 70435, NULL, NULL, 93);
+INSERT INTO filth.movie VALUES (4284, 'The Bourne Legacy', 2012, '***', 'PG-13', 'USA', NULL, 'tt1194173', 0, 49040, NULL, NULL, 135);
