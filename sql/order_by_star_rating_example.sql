6a23eac6f344068aa855101ad671d5cb66b7ded6
Tyler Hayes <thayes@amplify.com>
2018-09-13 12:01:46 -0700

    Add potential favorites lists and sample sql of order by rating

---
+++ sql/order_by_star_rating_example.sql
@@ -0,0 +1,11 @@
+﻿select m.title, m.year, m.star_rating, m.tmdb_id
+from filth.movie m
+join filth.tag_given_to tgt using (mid)
+join filth.tag t using (tid)
+where t.tag_name = 'action'
+and (m.star_rating = '****'
+or m.star_rating = '***½'
+or m.star_rating = '***')
+order by star_rating <> '****',
+         star_rating <> '***½',
+         star_rating <> '***';
