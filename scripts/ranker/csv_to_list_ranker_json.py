ef1d6f1ed95afb04c4d719a6b986a15cf1996c9b
Tyler Hayes <thayes@amplify.com>
2020-02-10 09:28:48 -0800

    Minor tweaks

---
+++ scripts/ranker/csv_to_list_ranker_json.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+Usage:
+    csv_to_list_ranker_json.py FILE TITLE
+
+Options:
+    -h --help       Show this screen.
+
+"""
+
+import sys
+import csv
+from os import path
+from docopt import docopt
+from util import getImageUrl
+
+csv.register_dialect('pipes', delimiter='|')
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    csvFile = args['FILE']
+    title = args['TITLE']
+
+    if not path.isfile(csvFile):
+        print(f'***ERROR: {csvFile} does not exist or is not a file')
+        sys.exit(1)
+
+    f = open(csvFile)
+    csv_lines = f.readlines()
+    f.close()
+    movies = csv.DictReader(csv_lines, dialect='pipes') # '|' as the field separator
+
+    print('{')
+    print('  "id": TO_BE_FILLED_IN,')
+    print(f'  "title": "{title}",')
+    print('  "items": [')
+
+    for movie in movies:
+        try:
+            imageUrl = getImageUrl(movie['tmdb_id'])
+        except Exception as e:
+            print(f'Exception: {e}')
+            continue
+        print('    {')
+        print(f'      "text": "{movie["title"]} ({movie["year"]})",')
+        print(f'      "imageUrl": "{imageUrl}"')
+        print('    },')
+
+    # close out the json
+    print('  ]')
+    print('}')
---
+++ scripts/ranker/util.py
@@ -0,0 +1,36 @@
+import requests
+import time
+from os import getenv
+
+TMDB_API_KEY = getenv('TMDB_API_KEY')   #environment variable TMDB_API_KEY required
+
+
+def getImageUrl(tmdbId):
+    # be nice to TMDB
+    time.sleep(0.5)
+
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/' + tmdbId + '/images',
+        params={'api_key': TMDB_API_KEY}
+    )
+    if response.status_code != 200:
+        message = '\nERROR from tmdb request for "' + tmdbId + '": ' + response.text + '\n'
+        raise Exception(message)
+    else:
+        try:
+            response_json = response.json()
+            posters = response_json['posters']
+            if not posters:
+                message = '\nERROR no posters for "' + tmdbId + '": ' + response.text + '\n'
+                print(message)
+                raise Exception(message)
+            else:
+                filePath = posters[0]['file_path']
+                return 'https://image.tmdb.org/t/p/w185/' + filePath
+        except KeyError:
+            message = '\nERROR missing "posters" key in json for "' + tmdbId + '": ' + response.text + '\n'
+            raise Exception(message)
+
+def getImdbUrl(movie):
+    imdbId = movie['imdb_id']
+    return f'http://www.imdb.com/title/{imdbId}'
